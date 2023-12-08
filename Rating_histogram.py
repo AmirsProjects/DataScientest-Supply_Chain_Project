a57c95774d13daac500bfe038fcc096f1f1833fd
AmirsProjects <153244700+AmirsProjects@users.noreply.github.com>
2023-12-08 02:24:05 +0100

    Update Rating_histogram.py

--- Rating_histogram.py
+++ Rating_histogram.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-df_sorted = review_data.sort_values(by ='Rating', ascending = True)
-plt.hist(df_sorted.Rating,  bins = 5, color ='Blue', rwidth=0.9, label = "Rating");
+## Drop missing values
+review_data.dropna(inplace=True)
+
+df = review_data.sort_values(by ='Rating', ascending = True)
+plt.hist(df.Rating,  bins = 5, color ='Blue', rwidth=0.9, label = "Rating");
 plt.legend()
