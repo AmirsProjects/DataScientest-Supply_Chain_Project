438c05c2764d23edf34e6a6c0b89438394073f4b
AmirsProjects <153244700+AmirsProjects@users.noreply.github.com>
2023-12-08 02:06:53 +0100

    Update Rating_histogram.py

--- Rating_histogram.py
+++ Rating_histogram.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+
 df_sorted = review_data.sort_values(by ='Rating', ascending = True)
 plt.hist(df_sorted.Rating,  bins = 5, color ='Blue', rwidth=0.9, label = "Rating");
 plt.legend()
